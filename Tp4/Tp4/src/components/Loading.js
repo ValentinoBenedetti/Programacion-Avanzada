c30f676e8f201c976be231011bd7217b2a410885
Valentino Benedetti <valentinobenedetti9@gmail.com>
2025-09-19 13:02:32 -0300

    Eliminar duplicado de carpeta Tp4

--- Tp4/Tp4/src/components/Loading.js
+++
@@ -1,9 +0,0 @@
-export function createLoading() {
-  const loadingElement = document.createElement('div');
-  loadingElement.className = 'loading';
-  loadingElement.innerHTML = `
-    <div class="spinner"></div>
-    <p>Cargando lanzamientos...</p>
-  `;
-  return loadingElement;
-}
--- Tp4/Tp4/src/index.js
+++
@@ -1,6 +0,0 @@
-import './styles/main.css';
-import { App } from './app.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-  new App();
-});
